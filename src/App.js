24030eb03859dc9522c59da3ee8e1160bd52c9e0
Allison Hamburger <aghamburger@gmail.com>
2024-02-24 15:37:14 -0700

    Add some basic UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header className="App-body">
+        <p>What kind of pattern are you looking for?</p>
+        <div className="Search-container">
+          <textarea rows="4" cols="50" />
+          <button>Search</button>
+        </div>
       </header>
     </div>
   );
