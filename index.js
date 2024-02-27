e305e6017c76864531fdaa9d2e781af8b0981c50
Allison Hamburger <aghamburger@gmail.com>
2024-02-26 19:46:42 -0700

    Add cors setup to server

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const { generateRavelrySearchTerms } = require("./apis/openai");
 const { searchRavelry } = require("./apis/ravelry");
 
@@ -10,11 +11,22 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const allowlist = ['http://localhost:3000', 'https://ahamburger.github.io/knit-gen-ai']
+const corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
-app.get("/search", async (req, res) => {
+app.get("/search", cors(corsOptionsDelegate), async (req, res) => {
   const input = req.query.input;
   if (typeof input !== 'string') {
     res.status(400).send('Invalid user input')
